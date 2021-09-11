dad617e67a0a6b11a79412c8a62e06ba394b9786
RyanDynski <90189918+RyanDynski@users.noreply.github.com>
2021-09-11 14:12:33 -0400

    Add files via upload

---
+++ myfunc.cpp
@@ -0,0 +1,16 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: ryan
+ */
+#include <string>
+
+std::string func()
+{
+	//say hello
+	return "hello";
+}
+
+
+
---
+++ myfunc.h
@@ -0,0 +1,16 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: ryan
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <string>
+
+std::string func();
+
+
+
+#endif /* MYFUNC_H_ */
